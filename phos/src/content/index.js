654d176cef741edf5e805a8a3c0fbcc911b7077a
Brbb <fr.delisi@gmail.com>
2018-06-09 14:29:03 +0800

    Load image in placeholder

--- phos/src/content/index.js
+++ phos/src/content/index.js
@@ -1,4 +1,24 @@
 console.log('YO')
-var script = document.createElement('script');
-script.src = "https://code.jquery.com/jquery-3.3.1.min.js"
-document.getElementsByTagName('head')[0].appendChild(script);
+var fakeSourceUrl = 'https://bit.ly/'
+
+function loadImage(containerObject, urlCode) {
+
+    var img = document.createElement('img');
+    img.src = `${fakeSourceUrl}${urlCode}`;
+    img.style.width = '200px';
+
+    var figure = document.createElement('figure')
+    var figcaption = document.createElement('figcaption')
+    figcaption.textContent = containerObject.innerText
+    figure.appendChild(img)
+    figure.appendChild(figcaption)
+    containerObject.appendChild(figure)
+}
+
+var phosObject = $("p:contains('phos://')")[0]
+
+if (phosObject) {
+    var phosCode = phosObject.innerText.split('phos://')[1]
+
+    loadImage(phosObject, phosCode)
+}
